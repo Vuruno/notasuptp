442a1c5c1d04d3baa5dd19784fa621ad4656b658
brunobordond <bruno.daniel.bordon.diaz.uptp@gmail.com>
2022-01-27 12:39:54 -0300

    googleauth callback uri

--- settings/auth.js
+++ settings/auth.js
@@ -13,7 +13,7 @@ var router = express.Router();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback",
+  callbackURL: "https://uptpgrades.glitch.me/auth/google/callback",
   passReqToCallback: true
 },
   async function (request, accessToken, refreshToken, profile, done) {
