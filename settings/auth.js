89c875974170c3bd6e12534e605c0a07242e661b
brunobordond <bruno.daniel.bordon.diaz.uptp@gmail.com>
2022-01-28 09:29:41 -0300

    login corrected

--- settings/auth.js
+++ settings/auth.js
@@ -1,6 +1,5 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
-const RememberMeStrategy = require('passport-remember-me').Strategy;
 const User = require('../models/User');
 const mongoose = require('mongoose');
 const findOrCreate = require("mongoose-findorcreate");
@@ -25,23 +24,6 @@ passport.use(new GoogleStrategy({
     const newUser = new User({ googleId: profile.id, name: profile.name, email: profile.email, enrolled: [] })
     await newUser.save()
     return done(null, newUser)
-  }
-));
-
-passport.use(new RememberMeStrategy(
-  function(token, done) {
-    Token.consume(token, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      return done(null, user);
-    });
-  },
-  function(user, done) {
-    var token = utils.generateToken(64);
-    Token.save(token, { userId: user.id }, function(err) {
-      if (err) { return done(err); }
-      return done(null, token);
-    });
   }
 ));
 
