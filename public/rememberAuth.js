64c5e127a4bb888016c931faa3e68fc8bb7fb4c8
brunobordond <bruno.daniel.bordon.diaz.uptp@gmail.com>
2023-06-15 18:24:45 +0800

    1.0.15

--- public/rememberAuth.js
+++ public/rememberAuth.js
@@ -1,12 +1,15 @@
 //get Cookies
-let decodedCookie = decodeURIComponent(document.cookie)
+let decodedCookie = document.cookie
 decodedCookie = decodedCookie.split(';')
 let settled = '', user_id = ''
+console.log(document.cookie)
 
-for (x of decodedCookie) {
+for (let x of decodedCookie) {
+    console.log(x)
     if (x.split('=')[0].includes('settled')) settled = Number(x.split('=')[1].split(';')[0])
     else if (x.split('=')[0].includes('user_id')) user_id = x.split('=')[1].split(';')[0]
 }
+console.log("!!decodedCookie")
 
 let currentUri = window.location.href
 currentUri = window.location.href.split(':')
@@ -36,27 +39,24 @@ if ((isNaN(settled) || (Date.now() - settled) > 3) && currentUri != 'logout') {
         });
 }
 setTimeout(function () {
+    let user_id_settled = !(user_id == null ||
+        user_id == undefined ||
+        user_id == 'undefined' ||
+        user_id == '' ||
+        user_id == "''" ||
+        user_id == '""')
+    let navbar_uptp = document.getElementById('myNaveNavBar').innerText == 'UPTP'
+
+    console.log("user_id_settled",user_id_settled,"navbar_uptp",navbar_uptp)
+    console.log("!user_id_settled && navbar_uptp",!user_id_settled && navbar_uptp)
+
     //Do not reload if user_id is not settled and navbar says 'UPTP'
-    if ((user_id == null ||
-        user_id == undefined ||
-        user_id == 'undefined' ||
-        user_id == '' ||
-        user_id == "''" ||
-        user_id == '""') ||
-        document.getElementById('myNaveNavBar').innerText == 'UPTP') {
-        console.log("NOMAN 01")
+    if (!user_id_settled && navbar_uptp)
         return
-    }
+        
     //Do not reload if user_id is settled and navbar does not says 'UPTP'
-    console.log("WTF bro 01")
-    if (!(user_id == null ||
-        user_id == undefined ||
-        user_id == 'undefined' ||
-        user_id == '' ||
-        user_id == "''" ||
-        user_id == '""') &&
-        document.getElementById('myNaveNavBar').innerText != 'UPTP') {
+    if (!navbar_uptp)
         return
-    }
+
     location.reload()
 }, 3000);
